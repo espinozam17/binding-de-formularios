9891fc50801923bf1b6a9a9b33195c80b74055c1
“espinozam17” <130848535+Carlos171090@users.noreply.github.com>
2024-06-27 19:09:43 -0400

    entrega

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>Registro de tarjeta de crédito</h1>
+    <TarjetaFormulario/>
+  </div>
+</template>
+
+<script>
+import TarjetaFormulario  from "./components/TarjetaFormulario.vue"
+export default {
+  name: 'App',
+  components: {
+    TarjetaFormulario
+  }
+}
+</script>
+
+<style> 
+ #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
